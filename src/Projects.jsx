90a58ff77c4db703f682e0b55851f7c70cb1ba03
Your Name <you@example.com>
2025-11-19 14:46:56 +0100

    Add GitHub link to Todo List project

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -6,7 +6,8 @@ function Projects() {
       id: 'netlify-todolist',
       name: 'Todo List App',
       description: 'A modern and responsive todo list application for managing daily tasks efficiently',
-      homepage: 'https://todolist-ugbadavid.netlify.app',
+      html_url: 'https://github.com/davidugba/todo-list',
+      homepage: 'https://davidugba.github.io/todo-list/',
       language: 'JavaScript',
       technologies: ['React', 'JavaScript', 'CSS3', 'Local Storage'],
       stargazers_count: 0,
@@ -49,7 +50,7 @@ function Projects() {
               if (project.id === 'netlify-todolist') {
                 return (
                   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
-                    <path d="M9 16.17L4.83 12l-1.42 1.41L9 19 21 7l-1.41-1.41z"/>
+                    <path d="M19 3h-4.18C14.4 1.84 13.3 1 12 1c-1.3 0-2.4.84-2.82 2H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm-7 0c.55 0 1 .45 1 1s-.45 1-1 1-1-.45-1-1 .45-1 1-1zm2 14H7v-2h7v2zm3-4H7v-2h10v2zm0-4H7V7h10v2z"/>
                   </svg>
                 );
               } else if (project.id === 'netlify-ecommerce') {
